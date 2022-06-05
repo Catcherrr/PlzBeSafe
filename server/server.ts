8651dd7cba3868a3269ecbfc746d6d9fc89aa333
jejecrunch <gkswlgp456@gmail.com>
2022-06-06 03:38:01 +0900

    mod: sequelize과 swagger 설정

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,40 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+import { sequelize } from './models';
 
+// init
 const app = express();
-const PORT = 1234;
+dotenv.config();
+const PORT: number = parseInt(process.env.PORT as string, 10);
+const HOST: string = process.env.HOST || 'localhost';
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/welcome', (req: Request, res: Response, next: NextFunction) => {
-    res.send('welcome!');
-});
-
-app.listen(PORT, () => {
+app.listen(PORT, HOST, async () => {
     console.log(`
   ################################################
   🛡️  Server listening on port: ${PORT}🛡️
   ################################################
 `);
+
+    await sequelize
+        .authenticate()
+        .then(async () => {
+            console.log('connection success');
+        })
+        .catch((e) => {
+            console.log('TT : ', e);
+        });
 });
+
+// swagger 설정
+const swaggerSpec = YAML.load(path.join(__dirname, './modules/swagger.yaml'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+import index from './routers';
+app.use(index);
